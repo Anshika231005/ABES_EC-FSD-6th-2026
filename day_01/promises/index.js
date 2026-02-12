de04710614c7ac2f454614bbfa6a7ce41a667e7d
Anshika231005 <anshikashps0786@gmail.com>
2026-02-12 11:53:09 +0530

    node js

--- day_01/promises/index.js
+++ day_01/promises/index.js
@@ -1,11 +1,34 @@
-const myPromise =new Promise((resolve, reject) => {
-    let marks=33;
-    if (marks >= 33) {
-        resolve("You are passed");
-    } else {
-        reject("You are failed");
-    }
-});
+
+// const myPromise = new Promise((resolve, reject) => {
+//     let marks = 33;
+//     if (marks > 33) {
+//         resolve("pass");
+//     } else {
+//         reject("fail");
+//     }
+// })
+// const f1 = async () => {
+// try {
+//     const msg = await myPromise;
+//     console.log(msg);
+// } catch (error) {
+//     console.log("some error occured..");
+// }
+// }
+// f1();
+
+const getData = async () => {
+}
+fetch("https://fakestoreapi.com/products")
+    .then(res =>res.json())
+    .then(data=>console.log(data))
+    .catch(error=>console.log("error"))
+// const output = getData();
+// console.log(output);
+
+
+
+// myPromise.then((msg) => console.log(msg))
+//     .catch((error) => console.log("Some error occured", error));
+
 // console.log(myPromise);
-myPromise.then((msg) =>console.log(msg)) 
-.catch ((err) =>console.log("error" ,err));
